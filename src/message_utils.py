378b8615de4b7893d3fafb6207a6dbd19dfd5670
Jeremy <75492743+MinhIT9@users.noreply.github.com>
2024-04-16 15:01:20 +0700

    Done edit remove

--- src/message_utils.py
+++ src/message_utils.py
@@ -83,7 +83,7 @@ async def main(client):
                 media = None  # Đặt lại media là None để loại bỏ nó
 
             if media:
-                sent_message = await client.send_file(channel_entity, media, caption=message_text, link_preview=False)
+                sent_message = await client.send_file(channel_entity, media, caption=message_text)
             else:
                 sent_message = await client.send_message(channel_entity, message_text, link_preview=False)
             print(f"Đã gửi tin nhắn tới {channel_id}")
@@ -132,31 +132,16 @@ async def main(client):
         text = event.message.text.strip()  # Loại bỏ khoảng trắng thừa
         media = event.message.media
         
-        # Tìm command bắt đầu bằng #
+        # Tìm command bắt đầu bằng "#"
         command_match = re.search(r'#\S*', text)
         if command_match:
             command = command_match.group(0)
         else:
             command = ''
 
-        # Nếu không phải lệnh "/rm", xử lý chỉnh sửa tin nhắn thông thường
-        if command.lower() != "/rm":
-            
-             # Cắt bỏ command khỏi nội dung tin nhắn để chỉnh sửa
-            modified_message_text = text.replace(command, '').strip()
-            
-             # Sử dụng hàm modify_message_text để thay đổi vị trí các dấu ** và __
-            modified_message_text = modify_message_text(modified_message_text)
-            print("modified_message_text sau edit: ", modified_message_text)
-            
-            message_relations = await fetch_message_relations(original_message_id)
-            if not message_relations:
-                print(f"Không tìm thấy mối quan hệ cho tin nhắn ID {original_message_id}")
-                return
-            await edit_messages_in_batches(client, message_relations, modified_message_text, media)
-        
-        elif command.lower() == "/rm":  # Kiểm tra nếu lệnh là "/rm"
-            # modified_message_text = text[:-len(command)].strip()  # Cắt bỏ lệnh "/rm" khỏi nội dung
+        # Kiểm tra nếu lệnh là "/rm"
+        if text.endswith("/rm"):
+            # Cắt bỏ lệnh "/rm" khỏi nội dung
             message_relations = await fetch_message_relations(original_message_id)
 
             if not message_relations:
@@ -175,16 +160,29 @@ async def main(client):
                     print(f"Deleted message ID {forwarded_message_id} in channel {channel_handle}")
                 except Exception as e:
                     print(f"Failed to delete message {forwarded_message_id} in channel {channel_handle}: {e}")
-             # Xóa tin nhắn gốc ở channel_0
+            
+            # Xóa tin nhắn gốc ở channel_0
             try:
                 await client.delete_messages(channel_0, [original_message_id])
                 print(f"Deleted original message ID {original_message_id} in channel_0.")
             except Exception as e:
                 print(f"Failed to delete original message in channel_0: {e}")
 
-             # Gọi hàm xóa mối quan hệ trên API
+            # Gọi hàm xóa mối quan hệ trên API
             await delete_message_relations(messageMaping_api, "1", str(original_message_id))  # "1" là ID tài nguyên trên API
             return  # Dừng xử lý để không chỉnh sửa nếu là lệnh xóa
+
+        else:
+            # Nếu không phải lệnh "/rm", xử lý chỉnh sửa tin nhắn thông thường
+            modified_message_text = text.replace(command, '').strip()
+            modified_message_text = modify_message_text(modified_message_text)
+            print("modified_message_text sau edit: ", modified_message_text)
+
+            message_relations = await fetch_message_relations(original_message_id)
+            if not message_relations:
+                print(f"Không tìm thấy mối quan hệ cho tin nhắn ID {original_message_id}")
+                return
+            await edit_messages_in_batches(client, message_relations, modified_message_text, media)
                     
     async def edit_messages_in_batches(client, message_relations, modified_message_text, media):
         tasks = []
@@ -205,7 +203,7 @@ async def main(client):
             if media:
                 await client.edit_message(channel_entity, int(forwarded_message_id), file=media, text=modified_message_text)
             else:
-                await client.edit_message(channel_entity, int(forwarded_message_id), text=modified_message_text)
+                await client.edit_message(channel_entity, int(forwarded_message_id), text=modified_message_text, link_preview=False)
             print(f"Đã chỉnh sửa tin nhắn {forwarded_message_id} trên kênh {channel_handle}")
         except Exception as e:
             print(f"Không thể chỉnh sửa tin nhắn: {e}")
