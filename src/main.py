d47704e8b1b6683286505fbf2908df4ee8574c87
Jeremy <75492743+MinhIT9@users.noreply.github.com>
2024-09-11 17:22:44 +0700

    Test

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 import asyncio
 import threading
 import signal
-from werkzeug.serving import make_server
+# from werkzeug.serving import make_server
 from telethon import TelegramClient
 from config import api_id, api_hash, bot_token, channel_mapping_api, channel_mapping_api_id, channel_mapping
 from api_utils import fetch_channel_mapping
@@ -11,8 +11,8 @@ from message_utils import main as message_main
 from App.flask_app import app
 
 client = TelegramClient('bot', api_id, api_hash)
-server = make_server('localhost', 5000, app)
-server_thread = threading.Thread(target=server.serve_forever)
+# server = make_server('localhost', 5000, app)
+# server_thread = threading.Thread(target=server.serve_forever)
 stop_event = threading.Event()
 
 async def start_bot():
@@ -34,21 +34,21 @@ async def stop_bot():
     await client.disconnect()
     print("Telegram Bot đã dừng.")
 
-def stop_flask():
-    print("Đang dừng Flask...")
-    server.shutdown()
-    print("Flask đã dừng.")
+# def stop_flask():
+#     print("Đang dừng Flask...")
+#     server.shutdown()
+#     print("Flask đã dừng.")
 
 def signal_handler(sig, frame):
     print("Đang dừng... vui lòng chờ")
     asyncio.create_task(stop_bot())
-    stop_flask()
+    # stop_flask()
     stop_event.set()
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
-    server_thread.start()
+    # server_thread.start()
     print("Flask server is running. Access it at: http://localhost:5000")
     loop = asyncio.get_event_loop()
     loop.run_until_complete(start_bot())
-    server_thread.join()
+    # server_thread.join()
